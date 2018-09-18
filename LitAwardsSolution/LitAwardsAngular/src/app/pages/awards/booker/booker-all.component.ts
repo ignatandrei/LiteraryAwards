cce9c6884df0459eb656b15453fda5f30ff67823
ignatandrei <ignatandrei@yahoo.com>
2018-09-17 19:29:26 -0500

    awards1

--- LitAwardsSolution/LitAwardsAngular/src/app/pages/awards/booker/booker-all.component.ts
+++ LitAwardsSolution/LitAwardsAngular/src/app/pages/awards/booker/booker-all.component.ts
@@ -6,12 +6,46 @@ import { BookerService } from '../../../@core/data/booker.service';
   templateUrl: './booker-all.component.html',
   styleUrls: ['./booker-all.component.scss']
 })
-export class BookerAllComponent implements OnInit {
+export class BookerAllComponent {
 
-  constructor(private  booker: BookerService) { }
-  nr=0;
-  ngOnInit() {
-    this.booker.CreateOrGetDb().then(()=>this.nr = this.booker.numberBookerAuthors());
+  constructor(private  service: BookerService) { 
+
+    var self=this;
+    this.service.CreateOrGetDb().then(()=>{
+      var data= self.service.getData();
+      data.map(it=> {
+        self.visibleBooks.set(it.name,false);
+        self.allBooks.set(it.name,null);
+        //this.loadBooks(true,it.name);
+      }
+        );
+      
+        self.source = data;
+    });
+
   }
+
+  source: any[];
+  allBooks = new Map<string,any[]>();
+  visibleBooks= new Map<string,boolean>();
+
+  public changeVisible(it:boolean, name: string){
+    this.loadBooks(true,name);
+    this.visibleBooks[name] = !this.visibleBooks[name] ;
+  }
+   public loadBooks(it:boolean, name: string){
+     
+    
+      if(this.allBooks.has(name) && this.allBooks[name] != null)
+      {
+        return ;
+      }
+      ;
+     var books=  this.service.callByName(name);      
+     this.allBooks[name]=books;
+     
+     
+  }
+  
 
 }
